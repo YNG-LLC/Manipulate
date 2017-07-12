e35942a402a7ae33b95b6b6bd18376a5a1e91e70
DanA <yngsoftwaredev@gmail.com>
2017-07-12 10:42:12 -0500

    removed trash, updated runUpdate

--- runUpdate.sh
+++ runUpdate.sh
@@ -3,6 +3,9 @@ cd /var/www/html/Manipulate/
 
 
 
-git fetch origin master
-git status
-git reset --hard origin/master
+#git fetch origin master
+#git status
+#git reset --hard origin/master
+#git pull origin master
+git  pull
+
